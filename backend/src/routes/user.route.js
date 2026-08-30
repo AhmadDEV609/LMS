93ab186b56ba29b50b0d9bb35364722c94fe6b1f
Muhammad Ahmad <ahmad.dev6635@gmail.com>
2026-08-31 02:56:38 +0500

    update backend

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,14 +2,14 @@ import { Router } from 'express'
 import { signup, login, logout, changePassword, userData, deleteProfile, updateImage, updateProfile, getuserData } from '../controllers/user.controller.js'
 import { upload } from '../middleware/multer.middleware.js'
 import { auth, refreshTokenAuth } from '../middleware/auth.middleware.js'
-import { authLimiter } from '../middleware/rateLimit.middleware.js'
+// import { authLimiter } from '../middleware/rateLimit.middleware.js'
 const user = Router()
 
 
-user.post('/signup', authLimiter, upload.single('image'), signup)
-user.post('/login', authLimiter, login)
+user.post('/signup', upload.single('image'), signup)
+user.post('/login', login)
 user.post('/logout', logout)
-user.post('/resetPassword', authLimiter, changePassword)
+user.post('/resetPassword', changePassword)
 user.delete('/delete/profile/', auth, deleteProfile)
 user.patch('/update/profile', auth, updateProfile)
 user.patch('/update/image', auth, upload.single('image'), updateImage)
