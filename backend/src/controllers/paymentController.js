a6934f8bd9d1ef5e64182f9493a7428dbeeea8dc
Muhammad Ahmad <ahmad.dev6635@gmail.com>
2026-09-07 05:16:23 +0500

    stripe url fix

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -40,7 +40,7 @@ export const createCheckoutSession = async (req, res) => {
                 courseId: courseId.toString(),
             },
 
-            success_url: `${process.env.CLIENT_URL}/my/enrollment`,
+            success_url: `${process.env.CLIENT_URL}`,
             cancel_url: `${process.env.CLIENT_URL}/cancel`,
         });
 
